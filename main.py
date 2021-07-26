6722423c614b8a01ccbb7e3ae08f5451316be1f9
Lee <lees_taylor_18@yahoo.com>
2021-07-26 22:30:36 +0100

    Added loading & padding data, and build model in main section

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import Sequential
+from keras.layers import Embedding, LSTM, Dense
 
 def explore_data(index=159, range_start=None, range_end=None):
     index_range = False
@@ -29,6 +32,22 @@ def explore_data(index=159, range_start=None, range_end=None):
 
 
 if __name__ == '__main__':
-    explore_data(6)
-    print()
-    explore_data(0, 159, 161)
+    """ Exploring imdb data """
+    # explore_data(6)
+    # print()
+    # explore_data(0, 159, 161)
+    """ Loading data and padding """
+    training_set, testing_set = imdb.load_data(num_words = 10000)
+    x_train, y_train = training_set
+    x_test, y_test = testing_set
+    x_train_padded = sequence.pad_sequence(x_train, maxlen = 100)
+    x_test_padded = sequence.pad_sequence(x_test, maxlen = 100)
+    """ Build model """
+    model = Sequential()
+    model.add(Embedding(input_dim = 10000, output_dim = 128))
+    model.add(LSTM(units=128))
+    model.add(Dense(units=1, activation='sigmoid'))
+    # if verbose: model.summary()
+    model.compile(loss='binary_crossentropy', optimizer='SGD')
+
+    pass
